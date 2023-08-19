625af19a84f009f289dc8eeffc83b11e8b77cdf8
amit-vis <amit200698@gmail.com>
2023-08-19 11:38:45 +0530

    change

--- routes/api/v2/index.js
+++ routes/api/v2/index.js
@@ -1,9 +1,9 @@
 const express           = require('express');
 const router            = express.Router();
 
-router.get('/email',require('./email'));
-router.get('/name', require('./name'));
-router.get('/DOB', require('./dateofbirth'));
+router.use('/email',require('./email'));
+router.use('/name', require('./name'));
+router.use('/DOB', require('./dateofbirth'));
 
 
 module.exports=router;
